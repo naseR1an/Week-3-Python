6831e7bf704f37b4a848f5fc8bdb04c1c11ca0d0
naseR1an <joyserian8641@gmail.com>
2025-04-17 12:22:28 +0300

    Week 3 Python Assignment

--- answers.py
+++ answers.py
@@ -1,1 +1,15 @@
+# Question one
+def calculate_discount(price, discount_percent):
+    """Calculate final price after applying discount if it's 20% or higher."""
+    if discount_percent >= 20:
+        final_price = price - (price * (discount_percent / 100))  # Apply discount
+        return final_price
+    else:
+        return price  # Return original price 
+
+# Question two
+price = int(input("Enter the price of the item: "))
+discount_percent = int(input("Enter the discount percentage: "))
+print (f"The final price is: {calculate_discount(price, discount_percent)}")
+
 
